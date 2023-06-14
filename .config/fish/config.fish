5b0e637ea505b9683a118306731690bd6eb3828c
William Sjökvist <william.sjokvist@gmail.com>
2023-06-14 15:34:30 +0200

    updated dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,13 @@
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+set -x GOPROXY https://proxy.golang.org,direct
+set -x GOSUMDB off
+set -x GOPRIVATE github.com/shipwallet
+
+fish_add_path /opt/homebrew/bin -a
+fish_add_path /Applications/Docker.app/Contents/Resources/bin -a
+fish_add_path /google-cloud-sdk/bin -a
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
